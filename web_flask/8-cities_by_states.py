ce823595d9689a61bbda8fb96e17025b1ae67a27
Zuhair Ahmed <78954298+ZuhairAhmed0@users.noreply.github.com>
2023-07-24 00:09:37 +0200

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-that starts a Flask web application
+starts a Flask web application
 """
-
 
 from flask import Flask, render_template
 from models import *
@@ -10,20 +9,17 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """
-    display the states and cities listed in alphabetical order
-    """
-    states = list(storage.all("State").values())
-    return render_template("8-cities_by_states.html", states=states)
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db():
-    """ close the storage on teardown """
-    storeage.close()
-
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
